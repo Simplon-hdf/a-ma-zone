828969abbbf4964e6456bc296c607bd8dbc40726
kaly <kev.callet@gmail.com>
2023-12-17 15:45:21 +0100

    feat: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [OrdersModule, UsersModule, ProductsModule],
+  imports: [OrdersModule, UsersModule, ProductsModule, AuthorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
